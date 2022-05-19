106b085bf4b0452512ac84478e7d9e0b0d5414c2
ryo_kurosu <dtrjs.kurosu.0510@gmail.com>
2022-05-19 17:34:57 +0900

    マイナーチェンジ（ht_test）

--- ht_test.py
+++ ht_test.py
@@ -2,10 +2,13 @@ import dt_algorithm_test
 import pathlib
 import openpyxl as excel
 import datetime
+import time
 import os
 
 rho_list = [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09]
 theta_list = [0.01, 0.02, 0.05, 0.08, 0.1, 0.15, 0.2, 0.25, 0.30, 0.5]
+# rho_list = [0.01, 0.05, 0.09]
+# theta_list = [0.05, 0.1, 0.2]
 
 
 def wright_columns(ws):
@@ -34,7 +37,7 @@ def prepare_output_file():
     # 出力用のファイルを準備
     now_time = datetime.datetime.now()
     y_m_d = make_dir(now_time)
-    date_time = now_time.strftime('%Y%m%d%H%M%S')
+    date_time = now_time.strftime('%Y%m%d-%H%M%S')
 
     file_name = f"outputfile/TEST/{y_m_d}/hyper_turning/{date_time}_ht_test.xlsx"
 
@@ -54,9 +57,14 @@ def main():
     # --
     for i, rho in enumerate(rho_list):
         for j, theta in enumerate(theta_list):
+            st_time = time.time()
+            print(f"rho: {rho}, theta: {theta}")
+
             ROCAUC_train_score, ROCAUC_test_score, BACC_train_score, BACC_test_score = dt_algorithm_test.main(rho, theta)
-            ws.cell(row=i + 2, column=i + 2).value = ROCAUC_test_score
-            ws_train_score.cell(row=i + 2, column=i + 2).value = ROCAUC_train_score
+            ws.cell(row=j + 2, column=i + 2).value = ROCAUC_test_score
+            ws_train_score.cell(row=j + 2, column=i + 2).value = ROCAUC_train_score
+            ed_time = time.time()
+            print("time: {:.1f}".format(ed_time - st_time))
 
     wb.save(wb_name)
 
