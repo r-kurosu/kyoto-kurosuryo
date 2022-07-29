a89b775199760b2cf483d964282443861afa5ce9
ryo_kurosu <dtrjs.kurosu.0510@gmail.com>
2022-07-29 09:22:11 +0900

    [add] alfa to separate 0-1 for xgb

--- xgb_cv.py
+++ xgb_cv.py
@@ -84,12 +84,21 @@ def use_xgboost(INPUT_CSV, INPUT_TXT, cv_times, max_depth, eta, boost, weight, s
 
             y_pred_proba_train = model.predict(xgb_train)
 
-            y_pred_train = np.where(y_pred_proba_train > 0.5, 1, 0)
+            # find best alfa
+            max_score = 0
+            for a in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
+                temp_score = balanced_accuracy_score(y_train, np.where(y_pred_proba_train > a, 1, 0))
+                if temp_score >= max_score:
+                    max_score = temp_score
+                    alfa = i
+            # print(alfa)
+
+            y_pred_train = np.where(y_pred_proba_train > alfa, 1, 0)
             auc_train_score = roc_auc_score(y_train, y_pred_proba_train)
             bacc_train_score = balanced_accuracy_score(y_train, y_pred_train)
 
             y_pred_proba = model.predict(xgb_test)
-            y_pred = np.where(y_pred_proba > 0.5, 1, 0)
+            y_pred = np.where(y_pred_proba > alfa, 1, 0)
             auc_test_score = roc_auc_score(y_test, y_pred_proba)
             bacc_test_score = balanced_accuracy_score(y_test, y_pred)
 
